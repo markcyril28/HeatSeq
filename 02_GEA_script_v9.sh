fe274b5a3e50275c4f12ece0cfde51978655651c
markcyril28 <100415431+markcyril28@users.noreply.github.com>
2025-09-19 00:53:43 +0800

    Bam file. Not Kept.

--- 02_GEA_script_v9.sh
+++ 02_GEA_script_v9.sh
@@ -72,7 +72,7 @@ setup_logging() {
 	# Set up logging and output redirection
 	# Prompt for BAM cleanup option after logging setup
 	#read -p "Do you want to keep the BAM files after StringTie assembly? (y/n) [default: y]: " keep_bam_global
-	keep_bam_global="${keep_bam_global:-y}"
+	keep_bam_global="${keep_bam_global:-n}"
 
 	mkdir -p "$LOG_DIR"
 	#rm -f "$LOG_DIR"/*.log
