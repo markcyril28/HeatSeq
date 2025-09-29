43ed33172c2ae37bafa8779663aae35548d6eb7d
markcyril28 <100415431+markcyril28@users.noreply.github.com>
2025-09-30 02:53:10 +0800

    for tarring.

--- a_GEA_run.sh
+++ a_GEA_run.sh
@@ -7,15 +7,17 @@ LOG_FILE="${LOG_FILE:-$LOG_DIR/pipeline_${RUN_ID}_script_log.log}"
 
 mkdir -p "$LOG_DIR"
 # Forces removal and ignores errors if files do not exist.
-rm -rf "$LOG_DIR"/*
+#rm -rf "$LOG_DIR"/*
 
 # Chmod and Converts the file to Unix line endings
 chmod +x ./*.sh
 dos2unix ./*.sh
 
 # Run the main pipeline script and log resource usage
-/usr/bin/time -v ./b_GEA_script_v9_HPC.sh >> "$LOG_FILE" 2>&1
+#/usr/bin/time -v ./b_GEA_script_v9_HPC.sh >> "$LOG_FILE" 2>&1
 
-/usr/bin/time -v ./4b_Method_2_HISAT2_De_Novo/a_stringtie_method_2.2_HPC.sh >> "$LOG_FILE" 2>&1
+#/usr/bin/time -v ./4b_Method_2_HISAT2_De_Novo/a_stringtie_method_2.2_HPC.sh >> "$LOG_FILE" 2>&1
 
-/usr/bin/time -v ./4b_Method_2_HISAT2_De_Novo/b_heatmap_METHOD2_RESULTS_matrices_HPC.R >> "$LOG_FILE" 2>&1
+#/usr/bin/time -v ./4b_Method_2_HISAT2_De_Novo/b_heatmap_METHOD2_RESULTS_matrices_HPC.R >> "$LOG_FILE" 2>&1
+
+tar -czvf 4b_Method_2_HISAT2_De_Novo_$(date +%Y%m%d_%H%M%S).tar.gz 4b_Method_2_HISAT2_De_Novo  # Archive all StringTie results for sharing or backup
