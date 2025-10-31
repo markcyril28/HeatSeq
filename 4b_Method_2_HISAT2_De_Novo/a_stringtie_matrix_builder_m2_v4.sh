fd1d4b387407629f4ddd963849847ec9a1e668d6
markcyril28 <mercadomarkcyril28@gmail.com>
2025-10-31 20:15:49 +0800

    Some changes.

--- 4b_Method_2_HISAT2_De_Novo/a_stringtie_matrix_builder_m2_v4.sh
+++ 4b_Method_2_HISAT2_De_Novo/a_stringtie_matrix_builder_m2_v4.sh
@@ -48,6 +48,7 @@ Gene_Groups_Boilerplates=(
     "SmelDMPs"
     "SmelGIFs"
     "SmelGRFs"
+    "Selected_GRF_GIF_Genes"
     #"SmelGIF_with_Cell_Cycle_Control_genes"
     #"SmelGRF_with_Cell_Cycle_Control_genes"
     #"SmelGRF-GIF_with_Best_Cell_Cycle_Control_Genes"
@@ -55,14 +56,14 @@ Gene_Groups_Boilerplates=(
 
 # Sample lists from different projects
 SRR_LIST_PRJNA328564=(
-    SRR3884685  # Radicles  [MERISTEMATIC]
-    SRR3884677  # Cotyledons  [MERISTEMATIC]
-    SRR3884675  # Roots  [MERISTEMATIC]
-    SRR3884690  # Stems  [MERISTEMATIC]
-    SRR3884689  # Leaves (vegetative growth)
+    SRR3884685  # Radicles
+    SRR3884677  # Cotyledons  
+    SRR3884675  # Roots  
+    SRR3884690  # Stems  
+    SRR3884689  # Leaves_vegetative 
     SRR3884684  # Senescent_leaves 
-    SRR3884686  # Buds_0.7cm  [MERISTEMATIC]
-    SRR3884687  # Opened_Buds  [MERISTEMATIC]
+    SRR3884686  # Buds_0.7cm  
+    SRR3884687  # Opened_Buds  
     SRR3884597  # Flowers 
     SRR3884679  # Pistils 
     SRR3884608  # Fruits_1cm 
@@ -97,13 +98,13 @@ SRR_LIST_SAMN28540068=(
 
 
 # Combined list of all samples across projects
-SRR_LIST_COMBINED_2=(
+SRR_LIST_COMBINED=(
     "${SRR_LIST_PRJNA328564[@]}" 
-    "${SRR_LIST_SAMN28540077[@]}" 
-    "${SRR_LIST_SAMN28540068[@]}"
+    #"${SRR_LIST_SAMN28540077[@]}" 
+    #"${SRR_LIST_SAMN28540068[@]}"
 )
 
-SRR_LIST_COMBINED=(
+SRR_LIST_COMBINED_OFF=(
     # Roots
     "SRR3884675"   # Roots_1 (PRJNA328564)
     "SRR20722229"  # Roots_2 (SAMN28540077)
@@ -118,6 +119,7 @@ SRR_LIST_COMBINED=(
     "SRR3884689"   # Leaves_1 (PRJNA328564)
     "SRR20722230"  # Leaves_2 (SAMN28540077)
     "SRR20722386"  # Leaves_3 (SAMN28540068)
+    "SRR3884684"   # Senescent_leaves (PRJNA328564)
 
     # Buds
     "SRR3884686"   # Buds_1 (PRJNA328564)
@@ -136,6 +138,19 @@ SRR_LIST_COMBINED=(
     "SRR3884631"   # Fruits_1 (PRJNA328564)
     "SRR2072232"   # Fruits_2 (SAMN28540077)
     "SRR20722387"  # Fruits_3 (SAMN28540068)
+    "SRR3884608"   # Fruits_1cm (PRJNA328564)
+    "SRR3884620"   # Fruits_Stage_1 (PRJNA328564)
+    "SRR3884642"   # Fruits_Skin_Stage_2 (PRJNA328564)
+    "SRR3884653"   # Fruits_Flesh_Stage_2 (PRJNA328564)
+    "SRR3884664"   # Fruits_Calyx_Stage_2 (PRJNA328564)
+    "SRR3884680"   # Fruits_Skin_Stage_3 (PRJNA328564)
+    "SRR3884681"   # Fruits_Flesh_Stage_3 (PRJNA328564)
+    "SRR3884678"   # Fruits_peduncle (PRJNA328564)
+
+    # Other organs
+    "SRR3884685"   # Radicles (PRJNA328564)
+    "SRR3884677"   # Cotyledons (PRJNA328564)
+    "SRR3884679"   # Pistils (PRJNA328564)
 )
 
 # Mapping from SRR IDs to organ names for matrix headers
@@ -144,34 +159,48 @@ declare -A SRR_TO_ORGAN=(
     ["SRR3884675"]="Roots_1"      # PRJNA328564
     ["SRR20722229"]="Roots_2"     # SAMN28540077
     ["SRR31755282"]="Roots_3"     # SAMN28540068
-    
+
     # Stems
     ["SRR3884690"]="Stems_1"      # PRJNA328564
     ["SRR20722227"]="Stems_2"     # SAMN28540077
     ["SRR20722384"]="Stems_3"     # SAMN28540068
-    
+
     # Leaves
     ["SRR3884689"]="Leaves_1"     # PRJNA328564
     ["SRR20722230"]="Leaves_2"    # SAMN28540077
     ["SRR20722386"]="Leaves_3"    # SAMN28540068
-
-    # Opened Buds
-    ["SRR3884687"]="Opened_Buds_1" # PRJNA328564
+    ["SRR3884684"]="Senescent_leaves" # PRJNA328564
 
     # Buds
     ["SRR3884686"]="Buds_1"       # PRJNA328564
     ["SRR21010466"]="Buds_2"      # SAMN28540077
     ["SRR20722297"]="Buds_3"      # SAMN28540068
-    
+
+    # Opened Buds
+    ["SRR3884687"]="Opened_Buds_1" # PRJNA328564
+
     # Flowers
     ["SRR3884597"]="Flowers_1"    # PRJNA328564
     ["SRR20722234"]="Flowers_2"   # SAMN28540077
     ["SRR23909863"]="Flowers_3"   # SAMN28540068
-    
+
     # Fruits
     ["SRR3884631"]="Fruits_1"	  # PRJNA328564
     ["SRR2072232"]="Fruits_2"     # SAMN28540077
     ["SRR20722387"]="Fruits_3"    # SAMN28540068
+    ["SRR3884608"]="Fruits_1cm"   # PRJNA328564
+    ["SRR3884620"]="Fruits_Stage_1" # PRJNA328564
+    ["SRR3884642"]="Fruits_Skin_Stage_2" # PRJNA328564
+    ["SRR3884653"]="Fruits_Flesh_Stage_2" # PRJNA328564
+    ["SRR3884664"]="Fruits_Calyx_Stage_2" # PRJNA328564
+    ["SRR3884680"]="Fruits_Skin_Stage_3" # PRJNA328564
+    ["SRR3884681"]="Fruits_Flesh_Stage_3" # PRJNA328564
+    ["SRR3884678"]="Fruits_peduncle" # PRJNA328564
+
+    # Other organs
+    ["SRR3884685"]="Radicles"     # PRJNA328564
+    ["SRR3884677"]="Cotyledons"   # PRJNA328564
+    ["SRR3884679"]="Pistils"      # PRJNA328564
 )
 
 # StringTie abundance file column indices (1-based)
