2d5f677135fc9f9f6434840d97fd1d71f7d37b48
markcyril28 <mercadomarkcyril28@gmail.com>
2025-11-13 03:30:50 +0800

    Fix formatting in init_setup.sh: add spaces for improved readability in find commands

--- init_setup.sh
+++ init_setup.sh
@@ -3,6 +3,6 @@
 # Chmod and Converts the file to Unix line endings
 chmod +x ./*.sh
 dos2unix ./*.sh
-find ./0_INPUT_FASTAs -type f \( -name "*.fa" -o -name "*.fasta"\) -exec dos2unix {} + 2>/dev/null || true
+find ./0_INPUT_FASTAs -type f \( -name "*.fa" -o -name "*.fasta" \) -exec dos2unix {} + 2>/dev/null || true
 find . -type f \( -name "*.txt" -o -name "*.sh" -o -name "*.py" -o -name "*.R" \) -exec dos2unix {} + 2>/dev/null || true
-find . -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" -o -name "*.R"\) -exec chmod +x {} + 2>/dev/null || true
+find . -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" -o -name "*.R" \) -exec chmod +x {} + 2>/dev/null || true
