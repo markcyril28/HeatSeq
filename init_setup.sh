0ac8f783c9847aae4fbb2db9b12115fa5177416b
markcyril28 <mercadomarkcyril28@gmail.com>
2026-01-19 20:40:15 +0800

    Replace init_setup.sh with git_init_setup.sh for improved setup process and Unix line endings conversion

--- init_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Chmod and Converts the file to Unix line endings
-chmod +x ./*.sh
-dos2unix ./*.sh
-find ./0_INPUT_FASTAs -type f \( -name "*.fa" -o -name "*.fasta" \) -exec dos2unix {} + 2>/dev/null || true
-find . -type f \( -name "*.txt" -o -name "*.sh" -o -name "*.py" -o -name "*.R" \) -exec dos2unix {} + 2>/dev/null || true
-find . -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" -o -name "*.R" \) -exec chmod +x {} + 2>/dev/null || true
