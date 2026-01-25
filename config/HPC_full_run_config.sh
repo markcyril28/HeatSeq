f3f15ebbf6d7b17d4b0061a7421850aca512987d
markcyril28 <mercadomarkcyril28@gmail.com>
2026-01-25 21:30:04 +0800

    TEST.

--- config/HPC_full_run_config.sh
+++ config/HPC_full_run_config.sh
@@ -76,7 +76,7 @@ gtf_file="${ALL_Smel_Genes_Full_Name_reformatted_GTF_FILE}"
 # FASTA Files for Analysis
 ALL_FASTA_FILES=(
 	# List of FASTA files to process
-	"0_INPUT_FASTAs/fasta/reference_genomes/All_Smel_Genes_Full_Name.fasta"
+	#"0_INPUT_FASTAs/fasta/reference_genomes/All_Smel_Genes_Full_Name.fasta"
 	"0_INPUT_FASTAs/fasta/reference_genomes/All_Smel_Genes_Full_Name_TEST.fasta"
 	#"0_INPUT_FASTAs/fasta/reference_genomes/Eggplant_V4.1_transcripts.function.fa"
 	#"0_INPUT_FASTAs/fasta/experimental/TEST.fasta"
